930f186f88c03bf022649606ef2bcd8105ab641d
nikharkesari <king.nike26@gmail.com>
2021-03-19 21:11:53 +0530

    color

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-function setup() {
-    createCanvas(displayWidth, displayHeight).position(0, 0);
+function windowResized() {
+    resiveCanvas(windowWidth, windowHeight)
 }
-
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
+}
+a = 0
 function draw() {
-    ellipse(mouseX, mouseY, 6, 6);
-    select('#elements').position(0, 0)
+    noStroke();
+    r = map(noise(a), 0, 1, 0, 255)
+    g = map(noise(a + 10), 0, 1, 0, 255)
+    b = map(noise(a + 20), 0, 1, 0, 255)
+    fill(r, g, b, 20);
+    a += 0.01
+    ellipse(randomGaussian(mouseX, 20), randomGaussian(mouseY, 20), 30, 30);
 }
